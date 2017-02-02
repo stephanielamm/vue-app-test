4a2bb35e804fd68c23b3eb446081092d8339e8b3
stephanielamm <slamm5@live.unc.edu>
2017-02-02 17:45:21 -0500

    $emit added.
    In-class.

---
+++ src/components/Counter.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="Counter">
+    <button class="btn" @click="increment">{{ value }}</button>
+  </div>
+</template>
+
+<script>
+import Counter from './components/Counter'
+
+
+export default {
+  data () {
+    return {
+      value: 0
+    }
+  },
+methods: {
+  increment () {
+    this.value++
+    this.$emit
+  }
+}
+}
+</script>
+
+<style>
+#app {
+
+}
+</style>
