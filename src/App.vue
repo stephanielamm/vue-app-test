eaeb370df513f2d1628321080d4304ffadaa2cfc
stephanielamm <slamm5@live.unc.edu>
2017-02-02 17:37:50 -0500

    Now sourcing from Counter.Vue
    In-class.

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,23 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <test></test>
-    <hello></hello>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
-import Test from './components/Test'
+import Counter from './components/Counter'
+
 
 export default {
   name: 'app',
   components: {
-    Hello,
-    Test
+    Counter
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
